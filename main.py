2ab226b2c37c89f74dc965735f26a2f7e0728e32
Chinaza  Okeke <104084551+ChinazaOkeke@users.noreply.github.com>
2022-06-13 17:18:39 +0000

    I sorted out the indentations

--- main.py
+++ main.py
@@ -15,23 +15,25 @@ while True:
   user_choice = input("Enter a choice\n['R', 'P', 'S']: ")                  
                       
   computer_choice = random.choice(possible_choices) 
+
+  if user_choice in possible_choices:
+  
+    print(f"You chose {user_choice}, computer chose {computer_choice}")
                       
-  print(f"You chose {user_choice}, computer chose {computer_choice}")
-                      
-  if user_choice == computer_choice:
+    if user_choice == computer_choice:
         print(f"Both players selected {user_choice}. It's a tie! Try again")
-  elif user_choice == "rock":
-        if computer_choice == "scissors":
+    elif user_choice == "R":
+        if computer_choice == "S":
             print("Rock smashes scissors! You win!")
         else:
             print("Paper covers rock! You lose.")
-  elif user_choice == "paper":
-        if computer_choice == "rock":
+    elif user_choice == "P":
+        if computer_choice == "R":
             print("Paper covers rock! You win!")
         else:
             print("Scissors cuts paper! You lose.")
-  elif user_choice == "scissors":
-        if computer_choice == "paper":
+    elif user_choice == "S":
+        if computer_choice == "P":
             print("Scissors cuts paper! You win!")
         else:
             print("Rock smashes scissors! You lose.")
